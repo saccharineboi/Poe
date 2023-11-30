9a0e53c679b9af7bd3661671636052d406381316
saccharineboi <saccharineboi@gmail.com>
2023-11-30 15:32:07 +0400

    Add useful frustum functions

--- src/Utility.hpp
+++ src/Utility.hpp
@@ -17,9 +17,23 @@
 #pragma once
 
 #include "Constants.hpp"
+#include "Suppress.hpp"
+
+SUPPRESS_WARNINGS()
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/matrix.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/common.hpp>
+ENABLE_WARNINGS()
 
 #include <GLFW/glfw3.h>
 #include <cmath>
+#include <vector>
+#include <cassert>
+#include <limits>
 
 namespace Poe::Utility
 {
@@ -42,5 +56,46 @@ namespace Poe::Utility
     inline bool FloatEquals(float a, float b, float epsilon = EPSILON)
     {
         return std::abs(a - b) <= epsilon;
+    }
+
+    ////////////////////////////////////////
+    inline glm::vec3 ComputeFrustumCenter(const std::vector<glm::vec4>& frustumCorners)
+    {
+        assert(8 == frustumCorners.size());
+
+        glm::vec3 center(0.0f);
+        for (const glm::vec4& corner : frustumCorners) {
+            center += glm::vec3(corner);
+        }
+        return center / static_cast<float>(frustumCorners.size());
+    }
+
+    ////////////////////////////////////////
+    inline glm::mat4 FitLightProjectionToFrustum(const glm::mat4& lightView,
+                                                 const std::vector<glm::vec4>& frustumCorners,
+                                                 float zMult)
+    {
+        float minX = std::numeric_limits<float>::max();
+        float maxX = std::numeric_limits<float>::lowest();
+        float minY = std::numeric_limits<float>::max();
+        float maxY = std::numeric_limits<float>::lowest();
+        float minZ = std::numeric_limits<float>::max();
+        float maxZ = std::numeric_limits<float>::lowest();
+
+        for (const glm::vec4& corner : frustumCorners) {
+            glm::vec4 p{ lightView * corner };
+
+            minX = std::min(minX, p.x);
+            maxX = std::max(maxX, p.x);
+            minY = std::min(minY, p.y);
+            maxY = std::max(maxY, p.y);
+            minZ = std::min(minZ, p.z);
+            maxZ = std::max(maxZ, p.z);
+        }
+
+        minZ = minZ < 0.0f ? minZ * zMult : minZ / zMult;
+        maxZ = maxZ < 0.0f ? maxZ / zMult : maxZ * zMult;
+
+        return glm::ortho(minX, maxX, minY, maxY, minZ, maxZ);
     }
 }
